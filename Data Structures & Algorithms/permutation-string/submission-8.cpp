1dcc05e4328cf2a96453af5f6d7326a8c02a5914
mzii19 <mhdziyad21@gmail.com>
2026-09-02 21:34:50 +0530

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector <int> window (26,0);
+        vector <int> need (26,0);
+
+        for(char c: s1){
+            need[c-'a']++;
+        }
+
+        int left=0;
+        for(int right=0;right<s2.size();right++){
+            window[s2[right]-'a']++;
+            if(right-left+1>s1.size()){
+                window[s2[left]-'a']--;
+                left++;
+            }
+            if(window==need){
+                return true;
+            }
+        }
+        return false;
+    }
+};
